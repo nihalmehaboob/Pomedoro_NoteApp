5349a42ec1c8f801680128312335d43dde87f03c
Mohamed NIHAL K <115793246+nihalmehaboob@users.noreply.github.com>
2026-07-10 00:30:08 +0530

    project set up

--- app/src/main/java/com/example/noteappliction/presentation/screens/SideBarLayout.kt
+++ app/src/main/java/com/example/noteappliction/presentation/screens/SideBarLayout.kt
@@ -1,7 +1,20 @@
 package com.example.noteappliction.presentation.screens
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
